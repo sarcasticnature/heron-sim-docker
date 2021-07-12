f43cb35a7e7ae9f5ca4cf8f8ae4f03a1591965a2
Jake Keller <jakeller3630@gmail.com>
2021-07-12 18:47:18 -0400

    Add shebangs to run scripts

--- run_basic.sh
+++ run_basic.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 docker run -it --rm --net=host --env="DISPLAY" --env="QT_X11_NO_MITSHM=1" \
     --volume="$XAUTHORITY:/root/.Xauthority:rw" --name="heron-sim-base" \
     --privileged heron-sim-basic $1
