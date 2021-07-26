bd70d32b59bf0ef5012ece75420399624e6716f0
Jake Keller <jakeller3630@gmail.com>
2021-07-26 13:26:18 -0400

    Fix run command container names

--- run_basic.sh
+++ run_basic.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker run -it --rm --net=host --env="DISPLAY" --env="QT_X11_NO_MITSHM=1" \
-    --volume="$XAUTHORITY:/root/.Xauthority:rw" --name="heron-sim-base" \
+    --volume="$XAUTHORITY:/root/.Xauthority:rw" --name="heron-sim-basic" \
     --privileged heron-sim-basic $1
--- run_custom.sh
+++ run_custom.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker run -it --rm --net=host --env="DISPLAY" --env="QT_X11_NO_MITSHM=1" \
-    --volume="$XAUTHORITY:/root/.Xauthority:rw" --name="heron-sim-base" \
+    --volume="$XAUTHORITY:/root/.Xauthority:rw" --name="heron-sim-custom" \
     --privileged heron-sim-custom $1
